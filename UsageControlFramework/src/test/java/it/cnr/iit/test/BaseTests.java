dba40e013847dfd89a9daf82dfc985c4762af862
alessandro.rosetti <alessandro.rosetti@iit.cnr.it>
2019-02-14 17:47:50 +0100

    [WIP] test situe for UCF

---
+++ UsageControlFramework/src/test/java/it/cnr/iit/test/BaseTests.java
@@ -0,0 +1,101 @@
+package it.cnr.iit.test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import javax.annotation.PostConstruct;
+import javax.xml.bind.JAXBException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import iit.cnr.it.ucsinterface.pap.PAPInterface;
+import iit.cnr.it.ucsinterface.pdp.PDPInterface;
+import iit.cnr.it.ucsinterface.pip.PIPCHInterface;
+import iit.cnr.it.ucsinterface.sessionmanager.SessionManagerInterface;
+import it.cnr.iit.usagecontrolframework.configuration.xmlclasses.Configuration;
+import it.cnr.iit.usagecontrolframework.configuration.xmlclasses.XMLPip;
+import it.cnr.iit.usagecontrolframework.contexthandler.ContextHandlerLC;
+import it.cnr.iit.usagecontrolframework.contexthandler.exceptions.MalformedObjectException;
+import it.cnr.iit.usagecontrolframework.proxies.PIPBuilder;
+import it.cnr.iit.usagecontrolframework.proxies.ProxyPAP;
+import it.cnr.iit.usagecontrolframework.proxies.ProxyPDP;
+import it.cnr.iit.usagecontrolframework.proxies.ProxySessionManager;
+import it.cnr.iit.xacmlutilities.policy.utility.JAXBUtility;
+import oasis.names.tc.xacml.core.schema.wd_17.PolicyType;
+import oasis.names.tc.xacml.core.schema.wd_17.RequestType;
+
+public abstract class BaseTests {
+    protected Logger log = (Logger) LoggerFactory.getLogger(MainTests.class);
+	
+	protected SessionManagerInterface getSessionManager(Configuration ucsConfiguration) {
+		SessionManagerInterface sessionManager = new ProxySessionManager(
+			    ucsConfiguration.getSessionManager());
+		assertNotNull(sessionManager);
+		return sessionManager;
+	}
+
+	protected ContextHandlerLC getContextHandler(Configuration ucsConfiguration) throws MalformedObjectException {
+		ContextHandlerLC contextHandler = new ContextHandlerLC(ucsConfiguration.getCh());
+		log.info("Context handler status : " + contextHandler.isOk());
+		return contextHandler;
+	}
+
+	protected PDPInterface getPDP(Configuration ucsConfiguration) {
+		PDPInterface pdp = new ProxyPDP(ucsConfiguration.getPdp());
+		assertNotNull(pdp);
+		return pdp;
+	}
+
+	protected PAPInterface getPAP(Configuration ucsConfiguration) {
+		PAPInterface pap = new ProxyPAP(ucsConfiguration.getPap());
+		assertNotNull(pap);
+		return pap;
+	}
+	
+	protected ArrayList<PIPCHInterface> getPIPS(Configuration ucsConfiguration) {
+		ArrayList<PIPCHInterface>  pips = new ArrayList<>();
+
+		for (XMLPip xmlPIP : ucsConfiguration.getPipList()) {
+			log.info("Loading pip");
+			PIPCHInterface pip = PIPBuilder.build(xmlPIP);
+			assertNotNull(pip);
+			pips.add(pip);
+		}
+		
+		return pips;
+	}
+	
+	protected RequestType getRequestType(String fileName) throws JAXBException, URISyntaxException, IOException {
+		return (RequestType) loadXMLFromFile(fileName, RequestType.class);
+	}
+	
+	protected PolicyType getPolicyType(String fileName) throws JAXBException, URISyntaxException, IOException {
+		return (PolicyType) loadXMLFromFile(fileName, PolicyType.class);
+	}
+	
+	protected Configuration getUCSConfiguration(String ucsConfigFile) throws JAXBException, URISyntaxException, IOException {
+		return (Configuration) loadXMLFromFile(ucsConfigFile, Configuration.class);
+	}
+	
+	private Object loadXMLFromFile(String fileName, Class<?> className) throws JAXBException, URISyntaxException, IOException {
+		String data = readResourceFileAsString(fileName);
+		return JAXBUtility.unmarshalToObject(className, data);
+	}
+	
+	protected String readResourceFileAsString(String resource) throws URISyntaxException, IOException {
+		ClassLoader classLoader = this.getClass().getClassLoader();
+		
+		log.info("Loading resource file : " + resource);
+		Path path = Paths.get(classLoader.getResource(resource).toURI());
+		byte[] data = Files.readAllBytes(path);
+		return new String(data);
+	} 
+	
+}
--- UsageControlFramework/src/test/java/it/cnr/iit/test/CoverageTest.java
+++ UsageControlFramework/src/test/java/it/cnr/iit/test/CoverageTest.java
@@ -1,12 +1,7 @@
 package it.cnr.iit.test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 
 import javax.annotation.PostConstruct;
@@ -14,8 +9,6 @@ import javax.xml.bind.JAXBException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -28,138 +21,47 @@ import iit.cnr.it.ucsinterface.pdp.PDPInterface;
 import iit.cnr.it.ucsinterface.pip.PIPCHInterface;
 import iit.cnr.it.ucsinterface.sessionmanager.SessionManagerInterface;
 import it.cnr.iit.usagecontrolframework.configuration.xmlclasses.Configuration;
-import it.cnr.iit.usagecontrolframework.configuration.xmlclasses.XMLPip;
 import it.cnr.iit.usagecontrolframework.contexthandler.ContextHandlerLC;
 import it.cnr.iit.usagecontrolframework.contexthandler.exceptions.MalformedObjectException;
-import it.cnr.iit.usagecontrolframework.entry.UsageControlFramework;
-import it.cnr.iit.usagecontrolframework.proxies.PIPBuilder;
-import it.cnr.iit.usagecontrolframework.proxies.ProxyPAP;
-import it.cnr.iit.usagecontrolframework.proxies.ProxyPDP;
-import it.cnr.iit.usagecontrolframework.proxies.ProxySessionManager;
-import it.cnr.iit.xacmlutilities.policy.utility.JAXBUtility;
-import oasis.names.tc.xacml.core.schema.wd_17.PolicyType;
-import oasis.names.tc.xacml.core.schema.wd_17.RequestType;
+
 
 @EnableConfigurationProperties
 @TestPropertySource(properties = "application-test.properties")
 @ActiveProfiles("test")
 @SpringBootTest
 @SpringBootConfiguration
-class CoverageTest {
-    private Logger log = (Logger) LoggerFactory.getLogger(CoverageTest.class);
+class MainTests extends BaseTests {
 
-	@Value("${ucsConfigFile}")
+	@Value("${ucs-config-file}")
 	private String ucsConfigFile;
 
-	// TODO array of strings
-	@Value("${policyFile}")
+	@Value("${policy-file}")
 	private String policyFile;
 
-	// TODO array of strings
-	@Value("${requestFile}")
+	@Value("${request-file}")
 	private String requestFile;
-	
-	// do we need these ?
-	private Configuration ucsConfiguration;
-	private ContextHandlerLC contextHandler;
-	private SessionManagerInterface sessionManager;
-	private PIPCHInterface pip;
-	private PDPInterface pdp;
-	private PAPInterface pap;
-	private ArrayList<PIPCHInterface> pips;
-	
-	UsageControlFramework usageControlFramework;
 	
 	@PostConstruct
 	private void init() {
 		log.info("Init coverage tests");			
 	}
 	
-	private String readResourceFileAsString(String resource) throws URISyntaxException, IOException {
-		ClassLoader classLoader = this.getClass().getClassLoader(); 
-		Path path = Paths.get(classLoader.getResource(resource).toURI());
-		byte[] data = Files.readAllBytes(path);
-		return new String(data);
-	} 
-	
-	private void initUCSConfig() {
-		loadUCSConfiguration();
-		loadContextHandler();
-		loadSessionManager();
-		loadPIPS();
-		
-		PDPInterface pdp = new ProxyPDP(ucsConfiguration.getPdp());
-		assertNotNull(pdp);
-
-		PAPInterface pap = new ProxyPAP(ucsConfiguration.getPap());
-		assertNotNull(pap);
-		
-		usageControlFramework = new UsageControlFramework();
-	}
-
-	private Object loadXMLFromFile(String fileName, Class<?> className) throws JAXBException, URISyntaxException, IOException {
-		String data = readResourceFileAsString(fileName);
-		return JAXBUtility.unmarshalToObject(className, data);
-	}
-	
-	private RequestType loadRequestType(String fileName) {
-		try {
-			return (RequestType) loadXMLFromFile(fileName, RequestType.class);
-		} catch (JAXBException | URISyntaxException | IOException e) {
-			fail("cannot load request xml");
-		}
-		
-		return null;
-	}
-	
-	private PolicyType loadPolicyType(String fileName) {
-		try {
-			return (PolicyType) loadXMLFromFile(fileName, PolicyType.class);
-		} catch (JAXBException | URISyntaxException | IOException e) {
-			fail("cannot load policy xml");
-		}
-		
-		return null;
-	}
-	
-	private void loadUCSConfiguration() {
-		try {
-			ucsConfiguration = (Configuration) loadXMLFromFile(ucsConfigFile, Configuration.class);
-		} catch (Exception e) {
-			fail("cannot initialize ucs configuration xml");
-			e.printStackTrace();
-		}
-	}
-
-	private void loadSessionManager() {
-		sessionManager = new ProxySessionManager(
-			    ucsConfiguration.getSessionManager());
-			assertNotNull(sessionManager);
-	}
-
-	private void loadContextHandler() {
-		try {
-			contextHandler = new ContextHandlerLC(ucsConfiguration.getCh());
-			log.info("Context handler status : " + contextHandler.isOk());
-		} catch (MalformedObjectException e) {
-			fail("context hanler error");
-		}
-	}
-	
-	private void loadPIPS() {
-		pips = new ArrayList<>();
-		for (XMLPip xmlPIP : ucsConfiguration.getPipList()) {
-			PIPCHInterface pip = PIPBuilder.build(xmlPIP);
-			assertNotNull(pip);
-			pips.add(pip);
-		}
-	}
-
 	@Test
 	@DisplayName("Init UCS Configuration")
-	void testInitConfiguration() {
-		initUCSConfig();
-		fail("Not yet implemented");
+	void testInitConfiguration() throws JAXBException, URISyntaxException, IOException, MalformedObjectException {
+		Configuration ucsConfiguration  = getUCSConfiguration(ucsConfigFile);
+				
+		SessionManagerInterface sessionManagerInterface = getSessionManager(ucsConfiguration);
+		
+		ArrayList<PIPCHInterface> pipArray = getPIPS(ucsConfiguration);
+				
+		PAPInterface pap = getPAP(ucsConfiguration);
+		
+		PDPInterface pdp = getPDP(ucsConfiguration);
+		
+		ContextHandlerLC contextHandler = getContextHandler(ucsConfiguration);
+
+		//usageControlFramework = new UsageControlFramework();
 	}
 
 }
