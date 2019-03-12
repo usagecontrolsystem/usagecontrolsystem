7d1d0fd22a6d72ff755c7398766f89727583b4a1
hexec <alessandro.rosetti@gmail.com>
2019-03-12 15:21:53 +0100

    XACMLUtilities: add test stub

---
+++ XACMLUtilities/src/test/java/it/cnr/iit/test/XACMLUtilitiesTest.java
@@ -0,0 +1,20 @@
+package it.cnr.iit.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class XACMLUtilitiesTest {
+
+    @Before
+    public void setUp() throws Exception {}
+
+    @After
+    public void tearDown() throws Exception {}
+
+    @Test
+    public void test() {
+        // fail( "Not yet implemented" );
+    }
+
+}
