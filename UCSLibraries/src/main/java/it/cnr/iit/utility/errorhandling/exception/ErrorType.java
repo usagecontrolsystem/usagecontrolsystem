2f6030aafa4272dac45905e0139f4f9fd16a402a
Tezcan Dilshener <tex@dilshener.de>
2019-04-03 19:01:09 +0200

    Added utilities to check business scenario preconditions

---
+++ UCSLibraries/src/main/java/it/cnr/iit/utility/errorhandling/exception/ErrorType.java
@@ -0,0 +1,11 @@
+package it.cnr.iit.utility.errorhandling.exception;
+
+public enum ErrorType {
+    SYSTEM_ERROR,
+    DATA_ERROR,
+    DAO_ERROR,
+    CONFIGURATION_ERROR,
+    PERMANENT_ERROR,
+    TRANSIENT_ERROR,
+    USAGE_ERROR,
+}
