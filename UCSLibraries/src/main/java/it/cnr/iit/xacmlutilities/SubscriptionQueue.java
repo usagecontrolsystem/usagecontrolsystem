6644ed534e028952cfade8d043c16458d6d1c1ca
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2019-04-03 16:59:50 +0200

    SubscriptionQueue: handle exceptions, add logger

--- UCSLibraries/src/main/java/it/cnr/iit/xacmlutilities/SubscriptionQueue.java
+++ UCSLibraries/src/main/java/it/cnr/iit/xacmlutilities/SubscriptionQueue.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2018 IIT-CNR
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy
  * of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -17,45 +17,48 @@ package it.cnr.iit.xacmlutilities;
 
 import java.util.Iterator;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.logging.Logger;
 
 /**
  * This is the class implementing the subscription queue.
- * 
+ *
  * <p>
  * The subscription queue is the queue in which the various PIPs put the
  * informations about which are the attributes to be monitored and eventually
  * the additional informations about those attributes.
- * 
+ *
  * <br>
  * In the former this queue was implemented using an home-made solution, in this
  * case instead we will use the LinkedBlockingQueue provided by JAVA.
  * </p>
- * 
+ *
  * @author antonio
  *
  */
 public class SubscriptionQueue<T> {
+    protected static final Logger log = Logger.getLogger( SubscriptionQueue.class.getName() );
+
     // the queue in which the various PIPs will put monitored attributes
     LinkedBlockingQueue<T> linkedBlockingQueue = new LinkedBlockingQueue<>();
 
     /**
      * Gets the head of the queue; waits until the queue is not empty
-     * 
+     *
      * @return the head of the queue
-     * 
+     *
      */
     public T pop() {
         try {
             return linkedBlockingQueue.take();
         } catch( InterruptedException ie ) {
-            ie.printStackTrace();
+            Thread.currentThread().interrupt();
             return null;
         }
     }
 
     /**
      * Pushes an object in the queue and notifies any blocked thread
-     * 
+     *
      * @param elem
      *          object to add in the queue
      */
@@ -63,13 +66,13 @@ public class SubscriptionQueue<T> {
         try {
             linkedBlockingQueue.put( elem );
         } catch( InterruptedException e ) {
-            e.printStackTrace();
+            Thread.currentThread().interrupt();
         }
     }
 
     /**
      * Checks if the queue is empty
-     * 
+     *
      * @return true if the queue is empty; false otherwise
      */
     public boolean empty() {
@@ -83,7 +86,7 @@ public class SubscriptionQueue<T> {
         Iterator<T> it = linkedBlockingQueue.iterator();
         int i = 0;
         while( it.hasNext() ) {
-            System.out.println( "Queue[" + i + "] = " + it.next().toString() );
+            log.info( "Queue[" + i + "] = " + it.next().toString() );
             i++;
         }
     }
