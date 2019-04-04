c899a241ed13b3e4038506eada21bf3c0d479a5f
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2019-04-04 16:59:19 +0200

    PEPRest: UPDATE springboot version

--- PEPRest/src/main/java/it/cnr/iit/peprest/PEPRestStarter.java
+++ PEPRest/src/main/java/it/cnr/iit/peprest/PEPRestStarter.java
@@ -22,7 +22,7 @@ import java.util.logging.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import it.cnr.iit.peprest.configuration.PEPRestConfiguration;
