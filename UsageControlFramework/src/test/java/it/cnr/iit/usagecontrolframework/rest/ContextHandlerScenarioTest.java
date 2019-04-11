139d69041ebbc486ad57821239561e08ab22725f
Tezcan Dilshener <tex@dilshener.de>
2019-04-11 14:37:06 +0200

    UCS-30 Templates for CH scenario tests

---
+++ UsageControlFramework/src/test/java/it/cnr/iit/usagecontrolframework/rest/ContextHandlerScenarioTest.java
@@ -0,0 +1,11 @@
+package it.cnr.iit.usagecontrolframework.rest;
+
+import org.junit.Ignore;
+
+import com.tngtech.jgiven.junit.ScenarioTest;
+
+@Ignore
+public class ContextHandlerScenarioTest
+        extends ScenarioTest<GivenMessage, WhenContextHandlerLCService, ThenMessage> {
+
+}
