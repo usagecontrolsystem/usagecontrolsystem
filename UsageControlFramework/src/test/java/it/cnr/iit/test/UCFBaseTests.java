55c302507f736ce8122a539df0214c5adf33797d
antoniolm23 <antoniolm23@gmail.com>
2019-04-11 18:35:35 +0200

    UCS-3 : Switchd from ArrayList to List also in mockups

--- UsageControlFramework/src/test/java/it/cnr/iit/test/UCFBaseTests.java
+++ UsageControlFramework/src/test/java/it/cnr/iit/test/UCFBaseTests.java
@@ -261,8 +261,8 @@ public class UCFBaseTests {
         PIPCHInterface pip = Mockito.mock( PIPCHInterface.class );
 
         Attribute attr = getNewAttribute( attrId, category, dataType, attrReturn );
-        ArrayList<Attribute> attributeList = new ArrayList<>( Arrays.asList( new Attribute[] { attr } ) );
-        ArrayList<String> attributeIdList = new ArrayList<>( Arrays.asList( new String[] { attrId } ) );
+        List<Attribute> attributeList = new ArrayList<>( Arrays.asList( new Attribute[] { attr } ) );
+        List<String> attributeIdList = new ArrayList<>( Arrays.asList( new String[] { attrId } ) );
 
         Mockito.when( pip.getAttributes() ).thenReturn( attributeList );
         Mockito.when( pip.getAttributeIds() ).thenReturn( attributeIdList );
