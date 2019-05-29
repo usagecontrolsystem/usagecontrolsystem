1f2f07f4141446586776f2b3b65205720f5e2753
Tezcan Dilshener <tezcan@dilshener.de>
2019-05-29 18:10:01 +0200

    UCS-99 : format name of method to make sonar happy

--- UCSLibraries/src/test/java/it/cnr/iit/AbstractPojoTest.java
+++ UCSLibraries/src/test/java/it/cnr/iit/AbstractPojoTest.java
@@ -10,7 +10,7 @@ import pl.pojo.tester.api.assertion.Method;
 public class AbstractPojoTest {
 
     @Test
-    public void Should_Pass_All_Pojo_Tests() {
+    public void shouldPassAllPojoTests() {
         assertPojoMethodsForAll( DefaultPackageFilter.forPackage( "it.cnr.iit" ) ).testing( Method.GETTER, Method.SETTER );
     }
 }
